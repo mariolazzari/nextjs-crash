b637635c07c587a888a131db2717871d2b52cf66
Mario Lazzari <mario.lazzari@italcoeng.com>
2021-01-27 09:29:35 +0100

    Created project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title> WebDev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+      <h1>Welcome to NextJS</h1>
+    </div>
+  );
+}
